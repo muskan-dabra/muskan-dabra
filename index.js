2159667e444ac4e2c7231df7558794e9373e90e5
Muskan <99002027+muskan-dabra@users.noreply.github.com>
2022-06-14 22:11:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
+//data come in vs terminal without email all undefine problem comes
 var express=require("express");
 var path=require("path");
+var fileUpload=require("express-fileupload");
 var mysql=require("mysql");
+const {allowedNodeEnvironmentFlags,getMaxListeners}=require("process");
+const e=require("express");
+//var nodemailer=require("nodemailer");
+const req=require("express/lib/request");
+const { info } = require("console");
 var app=express();
-app.listen(2001,function(){
-    console.log("Server Started");
+const port = process.env.PORT||6070;
+
+app.listen(port,function(){
+    console.log(`Server Started at ${port} and ready for listening requests from Client-chrome`);
 });
+
 app.use(express.static("public"));
-app.use(express.urlencoded({'extended':true}));
+const urlencodedparser= express.urlencoded({'extended':false});
+app.use(express.json());
 
 app.get("/",function(req,resp){
-    var homepage=path.join(__dirname+"public","index.html");
-    resp.sendFile(homepage);
-})
+
+    resp.sendFile(__dirname+"/public/index.html");
+});
 
 var firstDbCon=
  {
      host:"localhost",
      user:"root",
      password:"",
-     database:"project"
+     database:"realjava"
  }
 
  var dbcon=mysql.createConnection(firstDbCon);
@@ -26,91 +37,240 @@ var firstDbCon=
         if(err)
             console.log(err);
             else
-            console.log("congrats");
+            console.log("congrats...");
  });
+ app.use(express.urlencoded({'extended':true}));
+ 
+    app.use(fileUpload());
 
-app.get("/signup",(req,resp)=>{
+app.get("/signup-process",(req,resp)=>{
      console.log(req.query);
-
-     //resp.setHeader("content-Type","text/html");
-        // console.log(req.body);
         var dataAry=[req.query.email,req.query.pwd,req.query.contact,req.query.type];
-            dbcon.query("insert into profiles values(?,?,?,?,current_date())",dataAry,function(err)
+        console.log(dataAry);
+            dbcon.query("insert into users values(?,?,?,?,current_date())",dataAry,function(err)
             {
                     if(err)
-                    resp.send(err);
+                         resp.send(err);
                     else
-                   resp.send("Record saved Successfully");
+                         resp.send("Record saved Successfully");
             });
  });
+ 
+ app.get("/login",function(req,resp){
+    console.log(req.query);
+          dbcon.query("select * from users where email=? and pwd=?",[req.query.a,req.query.b],(err,result)=>{
+          console.log(result);
+                  if(err)
+                  resp.send(err);
+                  else
+                 resp.send(result);
+          });
+});
 
- app.post("/login",function(req,resp){
-     resp.sendFile(__dirname+"/public/dashcitizen.html");
- })
-
- app.post("/profile",function(req,resp){
-     resp.sendFile(__dirname+"/public/profile")
- })
-
- app.post("/requirement",function(req,resp){
-    resp.sendFile(__dirname+"/public/postrequest.html")
-})
-
-app.post("/save-process",function(req,resp){
-    console.log(req.body);
-    var filename="nopic copy.jpg";
+app.post("/save",(req,resp)=>{
+    
+     var fileName="nopic.png";
     if(req.files!=null)
     {
-        var destination=path.join(process.cwd(),"public","uploard",req.files.profilepic.name);
-        filename=req.files.profilepic.name;
-        
-        req.files.profilepic.mv(destination,function(err){
-            if(err)
-            console.log(err);
-            else
-            console.log("profile uploaded sucessfully");
-        })
-    }
-    else{
-        console.log(req.body);
-        var dataAry=[req.body.txtUid,req.body.txtName,req.body.txtMob,req.body.txtAdd,req.body.txtCity,req.body.txtState,filename];
-        dbcon.query("insert into profilesdata values(?,?,?,?,?,?,?)",dataAry,function(err){
-            if(err)
-            resp.send(err);
-            else
-            resp.send("record saved successfully");
-        })
-    }
-
-})
-
-app.post("/update-profile",(req,resp)=>
- {
-    console.log(req.body);
-
-    var fileName;
-    if(req.files!=null)
-     {
-     var destination=path.join(process.cwd(),"public","uploads",req.files.profilePic.name);
-    fileName=req.files.profilePic.name;
-
-    req.files.profilePic.mv(destination,function(err){
+    var destination=path.join(process.cwd(),"public","upload",req.files.pp.name);
+    fileName=req.files.pp.name;
+   
+    req.files.pp.mv(destination,function(err){
         if(err)
             console.log(err);
-        else
+            else
             console.log("File Uploaded Successfully");
     })
     }
     else
-        fileName=req.body.jasoos;
-        
-       var dataAry=[req.body.txtName,req.body.txtMob,req.body.txtAdd,req.body.txtCity,req.body.txtState,
-            fileName,req.body.txtUid];
+        console.log(req.body);
+    var dataAry=[req.body.txtpceid,req.body.name,req.body.mob,req.body.address,req.body.city,req.body.state,fileName];
+    console.log(dataAry);
+        dbcon.query("insert into profile values(?,?,?,?,?,?,?)",dataAry,function(err)
+        {
+                if(err)
+                     resp.send(err);
+                else
+                     resp.send("Record saved");
+        });     
+    }) 
 
-            dbcon.query("update profiles set name=?,number=?,address=?,city=?,state=?,visited=?,propicpath=?,idpicpath=? where emailid=?",dataAry,function(err){
-                    if(err)
-                    resp.send(err);
-                    else
-                   resp.send("Record Updated Successfully");
-            });
+ app.post("/update",(req,resp)=>
+ {
+     var fileName;
+     if(req.files!=null)
+     {
+         var destination=path.join(process.cwd(),"public","upload",req.files.pp.name)
+         fileName=req.files.pp.name;
+         req.files.pp.mv(destination,function(err)
+         {
+             if(err)
+             console.log(err);
+             else
+             console.log("Pic Uploaded Successfully")
+         })
+     }
+     else
+     fileName=req.body.jasoos;
+      var dataAry=[req.body.name,req.body.mob,req.body.address,req.body.city,req.body.state,fileName,req.body.txtpceid];
+     dbcon.query("update profile set name=?,mob=?,address=?,city=?,state=?,pp=? where txtpceid=?",dataAry,function(err)
+     {
+         if(err)
+         resp.send(err);
+         else
+         resp.send("Record Updated Successfully");
+     })
  })
+
+ app.get("/fetchARecord",(req,resp)=>{
+    // console.log(req.query.txtpceid);
+    dbcon.query("select * from profile where txtpceid=?",[req.query.txtpceid],(err,result)=>
+    {
+        if(err)
+            resp.send(err);
+        else
+            resp.send(result); //it send JSON Array
+    })
+ })
+
+
+ app.get("/post-req",(req,resp)=>
+ {
+    //  console.log(req.body);
+    var dataAry=[req.query.txtreid,req.query.cat,req.query.what,req.query.upto,req.query.location];
+//    console.log(dataAry);
+        dbcon.query("insert into request values(?,?,?,?,?,current_date())",dataAry,function(err)
+        {
+                if(err)
+                     resp.send(err);
+                else
+                     resp.send("Record saved");
+        });   
+ })
+ 
+ app.get("/btnchange",(req,resp)=>
+ {
+     dbcon.query("update users set pwd=? where email=?",[req.query.b],[req.query.a],(err,result)=>
+     {
+         if(err)
+         resp.send(err);
+         else
+         resp.send(result);
+     })
+ })
+ 
+app.post("/submit",(req,resp)=>{
+    var fileName1="nopic.png";
+    var fileName2="nopic.png"
+    if(req.files!=null)
+    {
+        var destination=path.join(process.cwd(),"public","upload",req.files.profilepic.name)
+        fileName1=req.files.profilepic.name;
+        req.files.profilepic.mv(destination,function(err)
+        {
+            if(err)
+            console.log(err);
+            else
+            console.log("Pic Uploaded Successfully")
+        })
+    }
+    if(req.files!=null)
+    {
+        var destination=path.join(process.cwd(),"public","upload",req.files.idpic.name)
+        fileName2=req.files.idpic.name;
+        req.files.idpic.mv(destination,function(err)
+        {
+            if(err)
+            console.log(err)
+            else
+            console.log("ID Uploaded Successfully")
+        })
+    }
+    var dataAry=[req.body.txtwpeid,req.body.txtname,req.body.txtmob,req.body.city,req.body.txtaddress,req.body.cat,req.body.x,req.body.shop,req.body.work,fileName1,fileName2];
+    dbcon.query("insert into workers values(?,?,?,?,?,?,?,?,?,?,?)",dataAry,function(err)
+    {
+        if(err)
+        resp.send(err);
+        else
+        resp.send("Record Saved Successfully");
+    })
+})
+
+app.get("/fetchCities",(req,resp)=>
+{
+    dbcon.query("select distinct city from profile",(err,result)=>
+    {
+        if(err)
+        resp.send(err);
+        else
+        resp.send(result);
+    })
+})
+
+app.get("/showAll",(req,resp)=>{
+    dbcon.query("select * from profile",(err,result)=>{
+        if(err)
+        resp.send(err);
+        else
+        resp.send(result);
+    })
+})
+///////////////angular //////////////////////////////////
+app.get("/fetchCitiesw",(req,resp)=>
+{
+    dbcon.query("select distinct city from workers",(err,result)=>
+    {
+        if(err)
+        resp.send(err);
+        else
+        resp.send(result);
+    })
+}) 
+
+app.get("/fetchCategories",(req,resp)=>
+{
+    dbcon.query("select distinct cat from workers",(err,result)=>{
+        if(err)
+        resp.send(err);
+        else
+        resp.send(result);
+    })
+})
+app.get("/cityandcate",(req,resp)=>{
+    dbcon.query("select * from workers where city=? and cat=?",[req.query.x,req.query.y],(err,result)=>{
+        if(err)
+        resp.send(err);
+        else
+        resp.send(result);
+    })
+})
+
+app.get("/fetchCitiespr",(req,resp)=>
+{
+    dbcon.query("select distinct location from request",(err,result)=>
+    {
+        if(err)
+        resp.send(err);
+        else
+        resp.send(result);
+    })
+}) 
+
+app.get("/fetchCategoriespr",(req,resp)=>
+{
+    dbcon.query("select distinct cat from request",(err,result)=>{
+        if(err)
+        resp.send(err);
+        else
+        resp.send(result);
+    })
+})
+
+app.get("/cityandcatef",(req,resp)=>{
+    dbcon.query("select * from request where location=? and cat=?",[req.query.x,req.query.y],(err,result)=>{
+        if(err)
+        resp.send(err);
+        else
+        resp.send(result);
+    })
+})
